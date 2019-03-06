ae9523c1fbc10c8e4ca712e47d31b692281045e5
ibrahimSouleiman <ibrahim.souleiman-mahamoud@etu.univ-amu.fr>
2019-03-06 23:43:15 +0100

    teste

--- src/guesswhat/data_provider/guesswhat_dataset.py
+++ src/guesswhat/data_provider/guesswhat_dataset.py
@@ -193,7 +193,7 @@ class Dataset(AbstractDataset):
 
                     
 
-                if len(games) > 50: break
+                # if len(games) > 50: break
 
         super(Dataset, self).__init__(games)
 
